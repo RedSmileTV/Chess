921b264d61c4d220dac6ebc934179155581f9923
RedSmile <lelledefeo@gmail.com>
2023-07-10 00:03:18 +0200

    removed lines

--- src/main/pieces/Bishop.java
+++ src/main/pieces/Bishop.java
@@ -16,13 +16,31 @@ public class Bishop extends Piece {
         if (isWhite()) return whiteIcon;
         else return blackIcon;
     }
-
     @Override
     public boolean isValidMove(Board board, int startX, int startY, int endX, int endY) {
         // Laufmechanik vom Läufer
         int rowDiff = Math.abs(endX - startX);
         int colDiff = Math.abs(endY - startY);
 
-        return rowDiff == colDiff;
+        if (rowDiff != colDiff) {
+            // The move is not diagonal, so it's not a valid bishop move
+            return false;
+        }
+        // Determine the direction of the move
+        int rowDirection = (endX - startX) / rowDiff;
+        int colDirection = (endY - startY) / colDiff;
+        // Iterate through the squares between the starting and ending positions
+        int currentX = startX + rowDirection;
+        int currentY = startY + colDirection;
+        while (currentX != endX || currentY != endY) {
+            if (board.getPiece(currentX, currentY) != null) {
+                // Wenn eine Figur im Weg ist
+                return false;
+            }
+            currentX += rowDirection;
+            currentY += colDirection;
+        }
+        // The move is valid
+        return true;
     }
 }
